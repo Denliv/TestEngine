62737cb19375d79df655df9ecd836b0c7b4bdd76
Дмитрий Долгушин <ddolgushin@yandex.ru>
2023-08-17 18:04:37 +0600

    + Заготовка решения тестового движка.

---
+++ MyUnit/MuUnit.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using SystemArithmetic.Tests;
+using MyUnit;
+
+var typeInfo = typeof(SumTest);
+
+TestRunner.OnTestFailure += (name, message) => Console.WriteLine($"Тест не пройден: " +
+    $"{name}{(string.IsNullOrWhiteSpace(message) ? string.Empty : $". Сообщение: {message}")}"); 
+
+TestRunner.Run(typeInfo);
---
+++ MyUnit/MyUnit/MyAssert.cs
@@ -0,0 +1,11 @@
+﻿namespace MyUnit
+{
+    public static class MyAssert
+    {
+        public static void True(bool actual)
+        {
+            if (!actual)
+                throw new MyAssertTestFailureException();
+        }
+    }
+}
